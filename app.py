07f4c18aba5b0a47880caa6f7099bde0000e2a5f
EmlyAlva <emilyemily458@gmail.com>
2024-04-15 22:46:32 -0500

    app sencilla

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title('Mi primera aplicación en la Web')
 nombre = st.text_input('Cual es tu nombre?')
 
 if nombre is not None:
-    st.write(f'Bienvenido {nombre} a mi primera app Web :D')
+    st.write(f'Bienvenido {nombre} a mi primera app Web')
